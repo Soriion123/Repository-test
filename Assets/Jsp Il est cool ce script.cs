74344af7332805f56d3d12ee91056886b217caf0
Robin FELDEN <rokoward69@gmail.com>
2025-11-07 15:57:18 +0100

    Push Robin

--- Assets/Jsp Il est cool ce script.cs
+++ Assets/Jsp Il est cool ce script.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 
+
 public class JspIlestcoolcescript : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        coucou mange mon paf corentin berlioz;
+        quand a toi erwan jtaime bien;
+        l'autre corentin t nul;
+        et toi julien bah arrete nintendo;
     }
 
     // Update is called once per frame
